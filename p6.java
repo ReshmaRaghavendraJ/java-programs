faf32ecd52bd16326c085fc41c7ace7de87a2f53
ReshmaRaghavendraJ <reshmaraghavendraJ@gmail.com>
2023-07-27 19:58:59 +0530

    Addition of two numbers, giving inputs through User input(Read 2 numbers at Runtime)

---
+++ p6.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.Scanner;
+class program6
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter two Numbers:");
+       int a=s.nextInt();
+       int b=s.nextInt();
+       int res=a+b;
+       System.out.println("Result is:"+res);
+    }
+}
