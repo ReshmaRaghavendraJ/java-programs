addde63ee8ec2d2e5bdb587a9fc109a1065f243b
ReshmaRaghavendraJ <reshmaraghavendraJ@gmail.com>
2023-07-27 20:03:29 +0530

    Find String Length(String is Immutable type)

---
+++ p8.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.Scanner;
+class program8
+{
+    public static void main(String[] args)
+    {
+        //String s="Big Port";
+       // int n=s.length();
+       Scanner s=new Scanner(System.in);
+       System.out.println("Enter String:");
+        String s1=s.nextLine();
+       int n=s1.length();
+        System.out.println(n);
+    }
+}
