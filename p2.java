5a140b87e020d4f861c86f8a8163b650c9db4122
ReshmaRaghavendraJ <reshmaraghavendraJ@gmail.com>
2023-07-27 19:47:43 +0530

    Simple Addition of Two Numbers

---
+++ p2.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class program2
+{
+    public static void main(String[] args)
+    {
+        int a=10,b=35;
+        int res=a+b;
+        System.out.println("Addition of "+a+" and "+b+" is: "+res);
+    }
+}
