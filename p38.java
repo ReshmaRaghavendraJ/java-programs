4314bffce0e5e48b162119436ed502054ea95ff8
ReshmaRaghavendraJ <reshmaraghavendraJ@gmail.com>
2023-08-07 15:39:55 +0530

    Including Date Package and Built-in function toString()

---
+++ p38.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+class p38
+{
+    public static void main(String[] args)
+    {
+        Date d=new Date();
+        System.out.println(d.toString());
+    }
+}
