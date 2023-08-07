f4e5b12fd51abf6a965eb363faf4311bc8b06d4a
ReshmaRaghavendraJ <reshmaraghavendraJ@gmail.com>
2023-08-07 15:38:38 +0530

    Converting Integer type to String Type

---
+++ p36.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class p36
+{
+    public static void main(String[] args)
+    {
+        int a=100;
+        String s=String.valueOf(a);
+        System.out.println(s);
+    }
+}
