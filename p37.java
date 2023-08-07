c9c0040c9b16a66ad71a7d4c6701d598b8855417
ReshmaRaghavendraJ <reshmaraghavendraJ@gmail.com>
2023-08-07 15:39:18 +0530

    Converting String to Primitive Datatype and again converting to String type.

---
+++ p37.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+class p37
+{
+    public static void main(String[] args)
+    {
+        String s="10",r="0.25455565";
+        Double d=Integer.parseInt(s)+Double.parseDouble(r);
+        String s1=String.valueOf(d);
+        System.out.println(s1);
+    }
+}
