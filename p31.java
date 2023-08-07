9166a4969b5b60c778b1f7264269c228eec76f8f
ReshmaRaghavendraJ <reshmaraghavendraJ@gmail.com>
2023-08-07 15:33:24 +0530

    startsWith()

---
+++ p31.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+class p31
+{
+    public static void main(String[] args)
+    {
+        String s="KA14565";
+       if(s.startsWith("RJ"))
+       System.out.println("Karnataka");
+       else
+       System.out.println("Some other State");
+    }
+}
