01076c1f6f6ba02948271716c3fbed0002133bba
ReshmaRaghavendraJ <reshmaraghavendraJ@gmail.com>
2023-08-07 15:37:14 +0530

    Converting String to Integer Datatypes

---
+++ p35.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+class p35
+{
+    public static void main(String[] args)
+    {
+        String s="1351241";
+        int n=Integer.parseInt(s);
+        System.out.println(n);
+        Float f=Float.parseFloat(s);
+        System.out.println(f);
+    }
+}
